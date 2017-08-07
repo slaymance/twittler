1b5975f2bc7b18c78b04f6a42c1fb28c61159c79
slaymance <shane.laymance@gmail.com>
2017-08-07 15:33:42 -0700

    Add timestamp to tweets

--- application.js
+++ application.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.text('@' + tweet.user + ': ' + tweet.message  + ' | created at ' + 
+                tweet.created_at);
     $tweet.appendTo($body);
     index -= 1;
   }
@@ -19,7 +20,8 @@ $(document).ready(function(){
       while(tweetCount < streams.home.length) {
         var tweet = streams.home[tweetCount];
         var $tweet = $('<div style="display: none;"></div>');
-        $tweet.text('@' + tweet.user + ': ' + tweet.message);
+        $tweet.text('@' + tweet.user + ': ' + tweet.message + ' | created at ' 
+                    + tweet.created_at);
         $tweet.prependTo($body).show(300);
         tweetCount += 1;
       }
