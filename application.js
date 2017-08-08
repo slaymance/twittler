a311d9b223227d712fba800dbb2fa7a6ee9bac0b
slaymance <shane.laymance@gmail.com>
2017-08-08 02:46:15 -0700

    Reformat how tweets are added and add updateTimestamp function

--- application.js
+++ application.js
@@ -5,10 +5,33 @@ $(document).ready(function(){
   function addTweet(index) {
     var tweet = streams.home[index];
     var $tweet = $('<div class="tweet" style="display: none;"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' | created on ' 
-                + tweet.created_at);
-    $tweet.insertAfter($('.tracker')).show(300);
+    var $tweetHeader = $('<div class="tweetHeader"><span class="tweetUser"></span><span class="divider"></span><span class="tweetTimestamp"></span></div>');
+    var $tweetMessage = $('<div class="tweetMessage"></span>');
+
+    $tweetHeader.find('.tweetUser').text('@' + tweet.user);
+    $tweetHeader.find('.divider').text(' | ');
+    $tweetHeader.find('.tweetTimestamp').attr('data-timestamp', tweet.created_at);
+    $tweetHeader.find('.tweetTimestamp').text($.timeago(tweet.created_at));
+    console.log(tweet.created_at);
+    $tweetMessage.text(tweet.message);
+
+    $tweet.insertAfter($('.tracker'));
+    $tweetHeader.appendTo($tweet);
+    $tweetMessage.insertAfter($tweetHeader);
+    $tweet.show(300);
   };
+
+  (function updateTimestamp() {
+    var tweetTimestamps = document.getElementsByClassName('tweetTimestamp');
+
+    for(var i = 0; i < tweetTimestamps.length; i++) {
+      var currentSpan = $(tweetTimestamps[i]);
+      var timestamp = new Date(currentSpan.data('timestamp'));
+      
+      currentSpan.text($.timeago(timestamp));
+    }
+    setTimeout(updateTimestamp, 6000);
+  })();
 
   (function getNewTweets() {
     while(tweetCount < streams.home.length) {
