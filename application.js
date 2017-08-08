43b60fef1d3cbcff5b22efb0a10cd199ed12f886
slaymance <shane.laymance@gmail.com>
2017-08-08 13:07:39 -0700

    Add preliminary filter by user feature

--- application.js
+++ application.js
@@ -9,10 +9,10 @@ $(document).ready(function(){
     var $tweetMessage = $('<div class="tweetMessage"></span>');
 
     $tweetHeader.find('.tweetUser').text('@' + tweet.user);
+    $tweetHeader.find('.tweetUser').attr('data-user', tweet.user);
     $tweetHeader.find('.divider').text(' | ');
     $tweetHeader.find('.tweetTimestamp').attr('data-timestamp', tweet.created_at);
     $tweetHeader.find('.tweetTimestamp').text($.timeago(tweet.created_at));
-    console.log(tweet.created_at);
     $tweetMessage.text(tweet.message);
 
     $tweet.insertAfter($('.tracker'));
@@ -20,18 +20,6 @@ $(document).ready(function(){
     $tweetMessage.insertAfter($tweetHeader);
     $tweet.show(300);
   };
-
-  (function updateTimestamp() {
-    var tweetTimestamps = document.getElementsByClassName('tweetTimestamp');
-
-    for(var i = 0; i < tweetTimestamps.length; i++) {
-      var currentSpan = $(tweetTimestamps[i]);
-      var timestamp = new Date(currentSpan.data('timestamp'));
-      
-      currentSpan.text($.timeago(timestamp));
-    }
-    setTimeout(updateTimestamp, 6000);
-  })();
 
   (function getNewTweets() {
     while(tweetCount < streams.home.length) {
@@ -52,4 +40,47 @@ $(document).ready(function(){
     }
     setTimeout(trackNewTweets, 1000);
   })();
+
+  (function updateTimestamp() {
+    var tweetTimestamps = document.getElementsByClassName('tweetTimestamp');
+
+    for(var i = 0; i < tweetTimestamps.length; i++) {
+      var $currentSpan = $(tweetTimestamps[i]);
+      var timestamp = new Date($currentSpan.data('timestamp'));
+
+      $currentSpan.text($.timeago(timestamp));
+    }
+    setTimeout(updateTimestamp, 60000);
+  })();
+
+  $('main').on('click', '.tweetUser', function() {
+    var clickedUser = $(this).data('user');
+    var tweetUsers = document.getElementsByClassName('tweetUser');
+
+    for(var i = 0; i < tweetUsers.length; i++) {
+      var $currentSpan = $(tweetUsers[i]);
+      var currentUser = $currentSpan.data('user');
+
+      if(currentUser !== clickedUser) {
+        $currentSpan.closest('.tweet').toggle('slow');
+      }
+    }
+  });
+
+  /*(function filterUsers() {
+    $('main').on('click', '.tweetUser', function() {
+
+      var clickedUser = $(this).data('user');
+      var tweetUsers = document.getElementsByClassName('tweetUser');
+
+      for(var i = 0; i < tweetUsers.length; i++) {
+        var $currentSpan = $(tweetUsers[i]);
+        var currentUser = $currentSpan.data('user');
+        if(currentUser !== clickedUser) {
+          $currentSpan.closest('.tweet').toggle('slow');
+        }
+      }
+      filterUsers();
+    });
+  })();*/
 });
